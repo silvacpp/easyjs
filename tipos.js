aa076c287dc17101084776fb4115f70d3753cb2b
silvacpp <silva.cpp@gmail.com>
2020-03-23 22:15:19 -0300

    Create tipos.js

---
+++ tipos.js
@@ -0,0 +1,50 @@
+
+
+// console.log('Usuario')
+// console.log(nome)
+// console.log('Idade')
+// console.log(idade)
+
+
+//Operadores Aritmeticos 
+var soma = 1 + 2
+var subtracao = 3 - 2
+var divisao = 3 / 3
+var multiplicacao = 3 * 3
+
+// console.log('Operadores aritmeticos')
+// console.log(soma, subtracao, divisao, multiplicacao)
+
+
+// Operadores de Texto
+var nome = "Lucas"
+var sobrenome = "Silva"
+var idade = 25
+var nomeCompleto = nome + " " + sobrenome
+// console.log(nomeCompleto + " idade:" + idade)
+
+
+// Operadores Logicos
+//  1 e 0     Passa energia, nao passa energia
+var verdadeiro = true //
+var falso = false //
+
+
+// Eu vou sair de casa se  (Corona tiver passado) e (nao tenho sintomas)
+var coronaPassou = true
+var naoTenhoSintomas = false
+
+var vouSairDeCasa = coronaPassou && naoTenhoSintomas
+// console.log('Posso sair de casa', vouSairDeCasa)
+// &&  DUAS CONDICOES TEM QUE SER VERDADEIRAS
+
+// Levo GuardaChuva -> Se estiver chovendo ou previsao de chuva
+var estaChovendo = false
+var previsaoDeChuva = false 
+//                                 ou 
+var levoGuardaChuva = estaChovendo || previsaoDeChuva
+//SO uma verdadeira
+console.log('levo guardachuva ', levoGuardaChuva)
+
+
+ 
